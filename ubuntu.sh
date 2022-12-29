070377747403b913edb7f9227cf4a229c3b33bfc
wznpp1 <wznpp1@hotmail.com>
2022-12-29 10:13:26 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 
-sudo apt-get update && sudo apt-get install -y curl unzip gcc g++ make git fuse supervisor screen
+apt-get update && apt-get install -y curl unzip gcc g++ make git fuse supervisor screen
 
 git clone https://github.com/wznpp1/azure_torrentfs.git
 
-sudo ln -s "/home/$USER/azure_torrentfs/app" /root/app
+ln -s "/home/$USER/azure_torrentfs/app" /root/app
 
 #torrentfs
-sudo source <(curl -L https://go-install.netlify.app/install.sh)
-sudo go install github.com/anacrolix/torrent/cmd/torrentfs@latest
-sudo cp /home/go/bin/torrentfs /root/app/anacrolix/torrentfs
-sudo chmod +x /root/app/anacrolix/torrentfs
+source <(curl -L https://go-install.netlify.app/install.sh)
+go install github.com/anacrolix/torrent/cmd/torrentfs@latest
+cp /home/go/bin/torrentfs /root/app/anacrolix/torrentfs
+chmod +x /root/app/anacrolix/torrentfs
 
-sudo rm -rf /root/.config/rclone/rclone.conf
-sudo mkdir -p /root/.config/rclone/
-sudo ln -s /root/app/rclone/rclone.conf /root/.config/rclone/rclone.conf
-sudo ln -s /root/app/shell /root/shell
+rm -rf /root/.config/rclone/rclone.conf
+mkdir -p /root/.config/rclone/
+ln -s /root/app/rclone/rclone.conf /root/.config/rclone/rclone.conf
+ln -s /root/app/shell /root/shell
 
 #rclone
-curl https://rclone.org/install.sh | sudo bash
+curl https://rclone.org/install.sh | bash
 mkdir -p /root/app/anacrolix/downloads
 mkdir -p /root/app/anacrolix/mnt
 
@@ -29,13 +29,13 @@ chmod +x /root/app/shell/webdav.sh
 chmod +x /root/app/shell/mount.sh
 
 #supervisor
-sudo mv /etc/supervisor/supervisord.conf /etc/supervisor/supervisord.conf.bak
-sudo ln -s /root/app/supervisor/supervisord.conf /etc/supervisor/supervisord.conf
-sudo systemctl stop supervisor.service
+mv /etc/supervisor/supervisord.conf /etc/supervisor/supervisord.conf.bak
+ln -s /root/app/supervisor/supervisord.conf /etc/supervisor/supervisord.conf
+systemctl stop supervisor.service
 
-sudo supervisord -c /etc/supervisor/supervisord.conf
+supervisord -c /etc/supervisor/supervisord.conf
 
-sudo systemctl restart supervisor.service
-sudo systemctl enable supervisor.service
+systemctl restart supervisor.service
+systemctl enable supervisor.service
 
 #supervisorctl update
